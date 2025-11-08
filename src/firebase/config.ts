31ee6e9460487fa1445dffb925c31188bd99585e
Hamdi CHTIWI <mr.hamdichtiwi@gmail.com>
2025-11-08 21:41:27 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDc7Mzap8r5nTNeM6x22wXp1b2Dsto6

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDc7Mzap8r5nTNeM6x22wXp1b2Dsto6ITU",
+  authDomain: "nextleveltn-5bbb4.firebaseapp.com",
+  projectId: "nextleveltn-5bbb4",
+  storageBucket: "nextleveltn-5bbb4.appspot.com",
+  messagingSenderId: "1071530286269",
+  appId: "1:1071530286269:web:3e7aa42213b027a6b92bd4",
+  measurementId: "G-MN8E7QCXN5"
 };
