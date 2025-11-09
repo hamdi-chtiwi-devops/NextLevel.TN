27ea6ce0dc65f7e55554a8f8c2e8d2f8f17c78e1
Hamdi CHTIWI <mr.hamdichtiwi@gmail.com>
2025-11-09 09:06:16 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https:
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
